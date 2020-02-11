16758341e0ece0fa6391e1d37e7d7aad07ae6e52
congngusi <tranchiencongtd@gmail.com>
2020-02-11 21:14:56 +0700

    scrool appear

--- js/main.js
+++ js/main.js
@@ -1,3 +1,31 @@
+// view_show
+const items = document.querySelectorAll('.show1, .grid');
+
+const isInViewport = el => {
+  const rect = el.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      ((window.innerHeight || document.documentElement.clientHeight) + 200) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const run = () =>
+  items.forEach(item => {
+    if (isInViewport(item)) {
+      item.classList.add('show');
+    }
+  });
+
+// Events
+window.addEventListener('load', run);
+window.addEventListener('resize', run);
+window.addEventListener('scroll', run);
+
+
+
 
 // Countdown valentine
 // Set ngay ma ta dem
